488457b5aa7b187f38b6f0bf8541f07e89d96edb
BelleLQ <59607658+BelleLQ@users.noreply.github.com>
2021-10-06 01:51:26 -0400

    Services

--- services/CustomersService.js
+++ services/CustomersService.js
@@ -1,21 +1,84 @@
-const customerModel = require('../models/customerModel');
+const customerModel = require('../models/customersModel');
 
-exports.creatACustomer=(req,res)=>{
-
+exports.createACustomer=(req,res)=>{
+    const customer = new customerModel(req.body);
+    customer.save()
+    .then(newCustomer=>{
+        res.json({
+            message:"The customer is created",
+            data: newCustomer
+        })
+    })
+    .catch(err=>{
+        res.status(500).json({
+            message: err
+        })
+    })
 }
 
 exports.readAllCustomers=(req,res)=>{
-
+    customerModel.find()
+    .then(customers=>{
+        res.json({
+            message: 'A list of all customers',
+            data: customers,
+            totalCustomers: customers.length
+        })
+    })
+    .catch(err=>{
+        res.status(500).json({
+            message: err
+        })
+    })
 }
 
 exports.readACustomer=(req,res)=>{
-
+    customerModel.findById(req.params.id)
+    .then(customer=>{
+        res.json({
+            message: `The customer with id ${req.params.id}`,
+            data: customer
+        })
+    })
+    .catch(err=>{
+        res.status(404).json({
+            message: `There is no customer with id ${req.params.id}`
+        })
+    })
 }
 
 exports.updateACustomer=(req,res)=>{
-
+    customerModel.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    .then(customer=>{
+        if(customer){
+            res.json({
+                message: `Customer with id ${req.params.id} is updated`,
+                data: customer
+            })
+        }
+        else{
+            res.status(404).json({
+                message:`There is no customer with id ${req.params.id}`
+            })
+        }
+    })
+    .catch(err=>{
+        res.status(500).json({
+            message: err
+        })
+    })
 }
 
 exports.deleteACustomer=(req,res)=>{
-    
+    customerModel.findByIdAndRemove(req.params.id)
+    .then(()=>{
+        res.json({
+            message:`The customer with id ${req.params.id} is deleted`
+        })
+    })
+    .catch(err=>{
+        res.status(500).json({
+            message: err
+        })
+    })
 }
--- services/ProductsService.js
+++ services/ProductsService.js
@@ -1,21 +1,83 @@
 const productsModel = require('../models/ProductsModel');
 
-exports.creatAProduct=(req,res)=>{
-
+exports.createAProduct=(req,res)=>{
+    const product = new productsModel(req.body);
+    product.save()
+    .then(newProduct=>{
+        res.json({
+            message: `A new product is created`,
+            data: newProduct
+        })
+    })
+    .catch(err=>{
+        res.status(500).json({
+            message:err
+        })
+    })
 }
 
 exports.readAllProducts=(req,res)=>{
-
+    productsModel.find()
+    .then(products=>{
+        res.json({
+            message: `A list of all products`,
+            data: products
+        })
+    })
+    .catch(err=>{
+        res.status(500).json({
+            message: err
+        })
+    })
 }
 
 exports.readAProduct=(req,res)=>{
-
+    productsModel.findById(req.params.id)
+    .then(product=>{
+        res.json({
+            message: `The product with id ${req.params.id}`,
+            data: product
+        })
+    })
+    .catch(err=>{
+        res.status(404).json({
+            message: `There is no product with id ${req.params.id}`
+        })
+    })
 }
 
 exports.updateAProduct=(req,res)=>{
-
+    productsModel.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    .then(product=>{
+        if(product){
+            res.json({
+                message: `product with id ${req.params.id} is updated`,
+                data: product
+            })
+        }
+        else{
+            res.status(404).json({
+                message:`There is no product with id ${req.params.id}`
+            })
+        }
+    })
+    .catch(err=>{
+        res.status(500).json({
+            message: err
+        })
+    })
 }
 
 exports.deleteAProduct=(req,res)=>{
-    
+    productsModel.findByIdAndRemove(req.params.id)
+    .then(()=>{
+        res.json({
+            message: `Product with id ${req.params.id} is deleted`
+        })
+    })
+    .catch(err=>{
+        res.status(500).json({
+            message: err
+        })
+    })
 }
